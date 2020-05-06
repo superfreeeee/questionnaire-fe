b738ea6b642c076c4f80ce5fa23d24111c291939
181250098 <181250098@smail.nju.edu.cn>
2020-05-06 15:55:44 +0800

    feat: 修改createPaper

--- src/views/editor/create/CreatePaper.vue
+++ src/views/editor/create/CreatePaper.vue
@@ -1,39 +1,58 @@
 <template>
-  <el-dialog 
-    :visible="createPaperVisible"
-    @close="close()"
+  <el-dialog
+          title="提示"
+          :visible="createPaperVisible"
+          :before-close="handleClose"
+          @close="close()"
+          width="30%"
   >
-    <h1>Create Paper</h1>
-    <el-button @click="createPaper()">create</el-button>
+    <span>创建一个新的问卷</span>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="set_createPaperVisible(false)">取 消</el-button>
+      <el-button type="primary" @click="createPaper()">确 定</el-button>
+    </span>
+
   </el-dialog>
 </template>
 
 <script>
-import { mapMutations, mapGetters } from 'vuex'
+  import { mapMutations, mapGetters } from 'vuex'
 
-export default {
-  name: 'CreatePaper',
-  computed: {
-    ...mapGetters([
-      'createPaperVisible'
-    ])
-  },
-  methods: {
-    ...mapMutations([
-      'set_createPaperVisible'
-    ]),
-    createPaper() {
-      if(this.$route.path !== '/editor/create') {
-        this.$router.push('/editor/create')
-      }
-      this.set_createPaperVisible(false)
+  export default {
+    name: 'CreatePaper',
+    computed: {
+      ...mapGetters([
+        'createPaperVisible'
+      ])
     },
-    close() {
-      // console.log(close)
-      this.set_createPaperVisible(false)
+    data(){
+      return {
+        dialogVisible: false
+      }
+    },
+    methods: {
+      ...mapMutations([
+        'set_createPaperVisible'
+      ]),
+      createPaper() {
+        if(this.$route.path !== '/editor/create') {
+          this.$router.push('/editor/create')
+        }
+        this.set_createPaperVisible(false)
+      },
+      handleClose(done) {
+        this.$confirm("确认关闭?").then(_ =>{
+          done();
+          _;
+        })
+                .catch(_ =>{_})
+      },
+      close() {
+        // console.log(close)
+        this.set_createPaperVisible(false)
+      }
     }
   }
-}
 </script>
 
 <style>
--- src/views/editor/overview/Overview.vue
+++ src/views/editor/overview/Overview.vue
@@ -1,15 +1,20 @@
 <template>
   <div class='overview'>
-    <h1>Overview</h1>
-    <div>
-      <el-button @click="createPaper()">create</el-button>
-    </div>
+    <el-row>
+      <el-col :span="24">
+        <span class="title" style="float: left; font-size: 30px; font-weight: 700">问卷列表</span>
+        <div>
+          <el-button type="primary" style="text-align: right" @click="createPaper()">创建问卷</el-button>
+          <el-divider></el-divider>
+        </div>
+      </el-col>
+    </el-row>
     <div class="papers" style="display: flex">
       <div
-        style="width: 200px; height: 200px; border: 1px black solid;"
-        v-for="(paper, index) in papers"
-        :key="index"
-        @click="checkPaperDetail(paper.paperId)"
+              style="width: 200px; height: 200px; border: 1px black solid;"
+              v-for="(paper, index) in papers"
+              :key="index"
+              @click="checkPaperDetail(paper.paperId)"
       >
         <h2>A paper</h2>
         <h3>paperId: {{ paper.paperId }}</h3>
@@ -19,35 +24,35 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
+  import { mapMutations } from 'vuex'
 
-export default {
-  name: 'Overview',
-  data() {
-    return {
-      papers: [{
-        paperId: 0,
-      },{
-        paperId: 1,
-      },{
-        paperId: 2,
-      }]
-    }
-  },
-  methods: {
-    ...mapMutations([
-      'set_createPaperVisible'
-    ]),
-    createPaper() {
-      console.log(this.set_createPaperVisible)
-      this.set_createPaperVisible(true)
+  export default {
+    name: 'Overview',
+    data() {
+      return {
+        papers: [{
+          paperId: 0,
+        },{
+          paperId: 1,
+        },{
+          paperId: 2,
+        }]
+      }
     },
-    checkPaperDetail(paperId) {
-      console.log(paperId)
-      this.$router.push({ name: 'monitor', params: { paperId }})
+    methods: {
+      ...mapMutations([
+        'set_createPaperVisible'
+      ]),
+      createPaper() {
+        console.log(this.set_createPaperVisible)
+        this.set_createPaperVisible(true)
+      },
+      checkPaperDetail(paperId) {
+        console.log(paperId)
+        this.$router.push({ name: 'monitor', params: { paperId }})
+      }
     }
   }
-}
 </script>
 
 <style>
