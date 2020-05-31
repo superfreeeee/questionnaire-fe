5b126fdb809444e5fb8ea3fc7d9a8876b800fe0f
superfree <181250003@smail.nju.edu.cn>
2020-05-31 14:36:13 +0800

    fix: modify usage of clipboard

--- src/views/editor/create/PaperLink.vue
+++ src/views/editor/create/PaperLink.vue
@@ -1,86 +1,90 @@
 <template>
   <div class="paperlink">
-      <div class="paperlink-header">
-        <span style="font-size: 25px">問卷發放</span>
+    <div class="paperlink-header">
+      <span style="font-size: 25px">問卷發放</span>
+    </div>
+    <div class="paperlink-URL">
+      <span style="display: block; margin-top: 20px">
+        問卷鏈接 :
+      </span>
+      <div id="paperURL">
+        <span> {{ paperURL }}</span>
       </div>
-      <div class="paperlink-URL">
-        <span style="display: block; margin-top: 20px">
-            問卷鏈接 : 
-        </span>
-        <div id="paperURL">
-            <span> {{ paperURL }}</span>
-        </div>
-        <el-button
-        class="copyBtn" 
-        type="primary" 
-        size="small" 
-        style="margin-top: 10px" 
+      <el-button
+        class="copyBtn"
+        type="primary"
+        size="small"
+        style="margin-top: 10px"
         @click="copyURL()"
         icon="el-icon-link"
-        :data-clipboard-text="this.paperURL"
-        >
+        :data-clipboard-text="paperURL"
+      >
         複製鏈接
-        </el-button>
-      </div>
-      <el-divider />
-      <div style="text-align: left; padding-left: 30px">
-        <el-button 
-        type="primary" size="small" 
+      </el-button>
+    </div>
+    <el-divider />
+    <div style="text-align: left; padding-left: 30px">
+      <el-button
+        type="primary"
+        size="small"
         @click="browsePaper(paperURL)"
-        icon="el-icon-view">
+        icon="el-icon-view"
+      >
         預覽問卷
-        </el-button>
-      </div>
-      <el-divider />
+      </el-button>
+    </div>
+    <el-divider />
   </div>
 </template>
 
 <script>
-import Clipboard from 'clipboard';
+import Clipboard from 'clipboard'
 export default {
   name: 'PaperLink',
   data() {
-      return {
-          paperId: -1,
-          paperURL: `http://localhost:8080/customer/${this.$route.params.paperId}`,
-          show: true,
-      }
+    return {
+      paperId: -1,
+      paperURL: `http://localhost:8080/customer/${this.$route.params.paperId}`,
+      show: true
+    }
   },
   mounted() {
-      this.paperId = this.$route.params.paperId
-      var copyBtn = document.getElementsByClassName("copyBtn")
-      this.clipboard = new Clipboard(copyBtn)
+    this.paperId = this.$route.params.paperId
+    this._cp = new Clipboard('.copyBtn')
+  },
+  destroyed() {
+    this._cp.destroy()
   },
   methods: {
-      copyURL() {
-          this.$message({
-              message: "複製成功!",
-              type: 'success'
-          });
-      },
-      browsePaper(paperURL) {
-          console.log(paperURL);
-          window.open(paperURL);
-      }
+    copyURL() {
+      this.$message({
+        message: '複製成功!',
+        type: 'success'
+      })
+    },
+    browsePaper(paperURL) {
+      console.log(paperURL)
+      window.open(paperURL)
+    }
   }
 }
 </script>
 
 <style>
 .paperlink {
-    height: 100%;
+  height: 100%;
 }
 
 .paperlink-header {
-    height: 70px;
-    line-height: 70px;
-    text-align: left;
-    padding-left: 30px;
-    border-bottom: 1px groove;
+  height: 70px;
+  line-height: 70px;
+  text-align: left;
+  padding-left: 30px;
+  border-bottom: 1px groove;
 }
 
 .paperlink-URL {
-    padding-left: 30px;
-    text-align: left;
+  padding-left: 30px;
+  text-align: left;
 }
 </style>
